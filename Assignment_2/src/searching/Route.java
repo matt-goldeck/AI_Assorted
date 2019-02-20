d95565d449b27ae21fa8c349e9bbaca4396f7ab0
Matt Goldeck <matt@goldeck.net>
2019-02-20 16:04:30 -0500

    greedy and UCS

--- Assignment_2/src/searching/Route.java
+++ Assignment_2/src/searching/Route.java
@@ -1,14 +1,30 @@
 package searching;
 
-public class Route {
+public class Route implements Comparable<Route>{
 	/*
 	 * An edge.
 	 */
-	int destination_position, weight;
+	int source_position, destination_position, weight;
 	
-	public Route(int destination_position, int weight) {
+	public Route(int source_position, int destination_position, int weight) {
 		this.destination_position = destination_position;
+		this.source_position = source_position;
 		this.weight = weight;
+	}
+	
+	// Override compareTo so we can use PriorityQueues
+	@Override
+	public int compareTo(Route other) {
+		if(this.weight < other.get_weight())
+			return -1;
+		else if(this.weight > other.get_weight())
+			return 1;
+		else
+			return 0;
+	}
+	
+	public void set_weight(int w) {
+		this.weight = w;
 	}
 	
 	public int get_weight() {
@@ -17,6 +33,10 @@ public class Route {
 	
 	public int get_destination() {
 		return destination_position;
+	}
+	
+	public int get_source() {
+		return source_position;
 	}
 
 }
